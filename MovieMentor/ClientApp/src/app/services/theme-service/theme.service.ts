8d3c7528e61c3ba9b4077b061b99df58d22c3643
Tania Topciov <taniatopciov@yahoo.com>
2022-03-06 15:40:19 +0100

    Added Custom Theme

---
+++ MovieMentor/ClientApp/src/app/services/theme-service/theme.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+
+  private _isDarkTheme = false;
+
+  public darkThemeSubject = new BehaviorSubject(false);
+
+  constructor() {
+    this._isDarkTheme = localStorage.getItem('theme') === 'Dark';
+    this.darkThemeSubject.next(this._isDarkTheme);
+  }
+
+  get isDarkTheme() {
+    return this._isDarkTheme;
+  }
+
+  set isDarkTheme(theme) {
+    this.darkThemeSubject.next(theme);
+    this._isDarkTheme = theme;
+    localStorage.setItem('theme', this._isDarkTheme ? 'Dark' : 'Light');
+  }
+}
