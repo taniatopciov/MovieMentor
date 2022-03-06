2f41723f79b65dfa95d8190c4aa66bb16232285c
Tania Topciov <taniatopciov@yahoo.com>
2022-03-06 15:41:46 +0100

    Deleted Unnecessary Components and Added Toggle for Theme in Nav Menu Component

--- MovieMentor/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ MovieMentor/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ThemeService} from "../services/theme-service/theme.service";
+import {OverlayContainer} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
-  isExpanded = false;
+export class NavMenuComponent implements OnInit {
 
-  collapse() {
-    this.isExpanded = false;
+  constructor(public themeService: ThemeService, private overlayContainer: OverlayContainer) {
   }
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  ngOnInit() {
+    this.themeService.darkThemeSubject.subscribe(darkMode => {
+      const darkClassName = 'dark-theme-mode';
+      if (darkMode) {
+        this.overlayContainer.getContainerElement().classList.add(darkClassName);
+      } else {
+        this.overlayContainer.getContainerElement().classList.remove(darkClassName);
+      }
+    });
   }
 }
